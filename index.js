ec9486381cb99e19da093b6bc30ce62a1738d820
unknown <baudinopierreolivier@gmail.com>
2023-07-26 09:45:00 +0200

    add var doc catch and test

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("test");
+var jReso = document.getElementById("j_reso");
+var oReso = document.getElementById("o_reso");
+var totResult = document.getElementById("tot_result");
+var ccPlyer = document.getElementById("cc-player");
+var ccOppo = document.getElementById("cc-oppo");
+var resPDice = document.getElementById("res_pdice");
+var resODice = document.getElementById("res_odice");
+var ccJAv = document.getElementById("cc-j-advantage");
+var ccOAv = document.getElementById("cc-o-advantage");
+
+// console.log(
+//   jReso,
+//   oReso,
+//   totResult,
+//   ccPlyer,
+//   ccOppo,
+//   resPDice,
+//   resODice,
+//   ccJAv,
+//   ccOAv
+// );
